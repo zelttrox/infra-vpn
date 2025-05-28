70ba98af3c953734441d5371faab0a6f14a1e45d
enzo <enzo.granana@ynov.com>
2025-05-28 16:19:03 +0200

    Fix Ubuntu install

--- src/install_deps.sh
+++ src/install_deps.sh
@@ -3,6 +3,8 @@ read -p "Are you using Ubuntu or Arch? (Ubuntu/Arch) " distr
 if [[ ${distr,,} == "ubuntu" || ${distr,,} == "u" ]]; then
     sudo apt update && sudo apt install wireguard
     sudo apt install python3-tk
+    mkdir /etc
+    mkdir /etc/wireguard
 elif [[ ${distr,,} == "arch" || ${distr,,} == "a" ]]; then
     sudo pacman -S wireguard-tools openresolv tk
 else 
